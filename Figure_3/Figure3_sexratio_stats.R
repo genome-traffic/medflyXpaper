259feaf15bff0bd56d5be73dbe7a7b7d6a077b76
filicado <KqUrDkTGrhD4zJMk>
2021-01-07 10:22:44 +0000

    fixed stats

--- Figure_3/Figure3_sexratio_stats.R
+++ Figure_3/Figure3_sexratio_stats.R
@@ -1,28 +1,31 @@
-setwd("/Users/nikolai/Desktop/Papers/Angela/CCstats")
-#dir.create("output_data/")
+setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
+getwd()
+
+# -- Stats PANEL A ----------------------------------------
 
 # --- data input
-srd<-read.table("input_data/Cas9_srd.txt", header=T, sep="\t")
+srd<-read.table("input_data/Cas9_srd_pooled_cross.txt", header=T, sep="\t")
 srd$cross <- factor(srd$cross,levels = c("Cas9.1", "Cas9.2","double","Cas12a.multiplex", "WT"))
 srd$Generation <- as.factor(srd$Generation)
-srd <- within(srd, strain <- relevel(strain, ref = 16))
+#srd <- within(srd, strain <- relevel(strain, ref = 16))
 
-# -- Stats PANEL A
-#srd <- subset(srd, strain == "1a")
-srd <- subset(srd, cross == "Cas9.2")
-srd <- subset(srd, Transgenic_Parent == "M")
+# choose strain
+srd <- subset(srd, strain == "1a")
 srd <- droplevels(srd)
 y<-cbind(srd$Male_progeny,srd$Female_progeny)
 model<-glm(y~Transgenic_Parent + as.factor(Generation), family=binomial, data=srd)
 print(summary(model))
 
-#OR
+# -- Stats PANEL E ----------------------------------------
 
-# # # -- Stats PANEL D
-# srd <- subset(srd, cross == "double" | cross == "WT")
-# srd <- droplevels(srd)
-# y<-cbind(srd$Male_progeny,srd$Female_progeny)
-# model<-glm(y~strain, family=binomial, data=srd)
-# print(summary(model))
+# --- data input
+ssrd<-read.table("input_data/Cas9_individual_cross.txt", header=T, sep="\t")
+ssrd$cross <- factor(ssrd$cross,levels = c("Cas9.1", "Cas9.2","double","Cas12a.multiplex", "WT"))
+ssrd$Generation <- as.factor(ssrd$Generation)
+ssrd <- within(ssrd, strain <- relevel(strain, ref = "2a+2a"))
+ssrd <- droplevels(ssrd)
 
-# # #plot(model2)
+y<-cbind(ssrd$Male_progeny,ssrd$Female_progeny)
+model<-glm(y~strain, family=binomial, data=ssrd)
+print(summary(model))
+
